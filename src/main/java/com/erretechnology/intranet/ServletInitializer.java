259c4852ab22eb770684635b7fb9d36096c8a1eb
PaoloFortErre <paolo.forteleoni@errepro.tech>
2021-03-08 17:04:23 +0100

    prova
    Commit1

--- src/main/java/com/erretechnology/intranet/ServletInitializer.java
+++ src/main/java/com/erretechnology/intranet/ServletInitializer.java
@@ -4,7 +4,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 public class ServletInitializer extends SpringBootServletInitializer {
-
+	//prova
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(IntranetApplication.class);
