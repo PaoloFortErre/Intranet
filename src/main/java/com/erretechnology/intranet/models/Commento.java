4b755afd649c4a73edcb8d8bd60f156828db73a7
albertozaffalon <alberto.zaffalon@errepro.tech>
2021-03-11 12:39:43 +0100

    Aggiunta classe commenti con repository e service

--- src/main/java/com/erretechnology/intranet/models/Commento.java
+++ src/main/java/com/erretechnology/intranet/models/Commento.java
@@ -1,83 +1,65 @@
-
 package com.erretechnology.intranet.models;
 
-import java.util.Set;
-
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "post")
-public class Post {
+@Table(name = "comment")
+public class Commento /*extends Post*/ {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id_commento")
 	private int id;
 	private String testo;
 	private long timestamp;
-	private boolean visibile;
 	@ManyToOne
 	@JoinColumn(name = "id_autore")
 	private Utente autore;
-	@OneToMany(mappedBy = "post")
-	private Set<Commento> setCommenti;
+	@ManyToOne
+	@JoinColumn(name = "id_post")
+	private Post post;
+	private boolean visibile;
 	
-	public Set<Commento> getSetCommenti() {
-		return setCommenti;
-	}
-
-	public void setSetCommenti(Set<Commento> setCommenti) {
-		this.setCommenti = setCommenti;
-	}
-
-	public Post(String testo) {
-		this.setTesto(testo);
-	}
-	
-	public Post() {}
-
-	public String getTesto() {
-		return testo;
-	}
-
-	public void setTesto(String testo) {
-		this.testo = testo;
-	}
-
 	public int getId() {
 		return id;
 	}
-
 	public void setId(int id) {
 		this.id = id;
 	}
-
 	public Utente getAutore() {
 		return autore;
 	}
-
 	public void setAutore(Utente autore) {
 		this.autore = autore;
 	}
-
-	public long getTimestamp() {
-		return timestamp;
+	public Post getPost() {
+		return post;
 	}
-
-	public void setTimestamp(long timestamp) {
-		this.timestamp = timestamp;
+	public void setPost(Post post) {
+		this.post = post;
 	}
-
 	public boolean isVisibile() {
 		return visibile;
 	}
-
 	public void setVisibile(boolean visibile) {
 		this.visibile = visibile;
+	}
+	public long getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(long timestamp) {
+		this.timestamp = timestamp;
+	}
+	public String getTesto() {
+		return testo;
+	}
+	public void setTesto(String testo) {
+		this.testo = testo;
 	}
 }
