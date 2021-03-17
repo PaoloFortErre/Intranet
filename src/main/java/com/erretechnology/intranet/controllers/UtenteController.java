054a0e546e1e8bfb099716a08e617713d85de79a
AlessandroLinguaErrePro <alessandro.lingua@errepro.tech>
2021-03-17 15:41:18 +0100

    update cambio password

--- src/main/java/com/erretechnology/intranet/controllers/UtenteController.java
+++ src/main/java/com/erretechnology/intranet/controllers/UtenteController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
@@ -59,7 +60,7 @@ public class UtenteController extends BaseController{
 	}
 
 	
-	@PostMapping(value = "cambioPassword")
+	@RequestMapping(value = "/cambioPassword", method = {RequestMethod.GET, RequestMethod.POST})
 	public ModelAndView cambioPassword() {
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("cambiaPassword");
