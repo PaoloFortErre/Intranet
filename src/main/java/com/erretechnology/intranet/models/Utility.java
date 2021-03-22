3693fc04eb4836e5d89acc2966233ba816fdf067
AlessandroLinguaErrePro <alessandro.lingua@errepro.tech>
2021-03-22 12:44:15 +0100

    cambio formato data e piccoli bugfix profiloadmin

--- src/main/java/com/erretechnology/intranet/models/Utility.java
+++ src/main/java/com/erretechnology/intranet/models/Utility.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.format.annotation.DateTimeFormat;
 
 public class Utility {
-	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	@DateTimeFormat(pattern = "dd-MM-yyyy")
 	private LocalDate date;
 
 	public LocalDate getDate() {
@@ -17,7 +17,7 @@ public class Utility {
 	public void setDate(LocalDate date) {
 		this.date = date;
 	}
-	
+
 	 public static String getSiteURL(HttpServletRequest request) {
 	        String siteURL = request.getRequestURL().toString();
 	        return siteURL.replace(request.getServletPath(), "");
