5e3366ca0ae9b5e5b1f4cf4610bfa74cabe0dd9d
MatteoLinari1 <80266880+MatteoLinari1@users.noreply.github.com>
2021-03-22 10:31:19 +0100

    Modifica salvataggio Immagine news

--- src/main/java/com/erretechnology/intranet/models/Cliente.java
+++ src/main/java/com/erretechnology/intranet/models/Cliente.java
@@ -4,9 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Inheritance;
 import javax.persistence.Table;
-import javax.persistence.InheritanceType;
 
 @Entity
 @Table(name = "client")
