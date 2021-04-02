07d3e8692f3f4ac5c50a245327b525e6d76a0643
MatteoLinari1 <80266880+MatteoLinari1@users.noreply.github.com>
2021-04-02 15:59:50 +0200

    Gestione post Linkedin

---
+++ src/main/java/com/erretechnology/intranet/controllers/LinkedinController.java
@@ -0,0 +1,36 @@
+package com.erretechnology.intranet.controllers;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.erretechnology.intranet.models.PostLinkedin;
+import com.erretechnology.intranet.repositories.RepositoryLinkedin;
+
+@Controller
+@RequestMapping("linkedin")
+public class LinkedinController extends BaseController {
+	@Autowired
+	private RepositoryLinkedin repoLinkedin;
+	
+	@GetMapping("/new")
+	public String form(Model model) {
+		model.addAttribute("linkedin", new PostLinkedin()); 
+		return "linkedinForm";
+	}
+	
+	@PostMapping(value = "/insert")
+	public String post(@ModelAttribute("post") PostLinkedin post, HttpSession session, ModelMap model) {
+		repoLinkedin.save(post);
+		saveLog("aggiunto un post linkedin", serviceDatiPersonali.findById(Integer.parseInt(session.getAttribute("id").toString())));
+
+		return "redirect:/homepage";
+	}
+}
