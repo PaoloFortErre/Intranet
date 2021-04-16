f51d846f908ec842e7fca432afb4b84ce4ac7016
AlessandroLinguaErrePro <alessandro.lingua@errepro.tech>
2021-04-16 10:26:22 +0200

    Server Deploy Ready

--- src/main/java/com/erretechnology/intranet/IntranetApplication.java
+++ src/main/java/com/erretechnology/intranet/IntranetApplication.java
@@ -2,12 +2,22 @@ package com.erretechnology.intranet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class IntranetApplication {
+public class IntranetApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
+	public static void main (String[] args) {
 		SpringApplication.run(IntranetApplication.class, args);
 	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder 
+	   application) {
+	      return application.sources(IntranetApplication.class);
+	   }
+	
+	
 
 }
