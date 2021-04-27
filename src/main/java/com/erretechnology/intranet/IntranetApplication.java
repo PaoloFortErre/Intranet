833ecb3c6b9913dade3a1745383670e841b6dff0
AlessandroLinguaErrePro <alessandro.lingua@errepro.tech>
2021-04-27 14:24:22 +0200

    Update IntranetApplication.java

--- src/main/java/com/erretechnology/intranet/IntranetApplication.java
+++ src/main/java/com/erretechnology/intranet/IntranetApplication.java
@@ -8,15 +8,17 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class IntranetApplication extends SpringBootServletInitializer {
 
-	public static void main (String[] args) {
-		SpringApplication.run(IntranetApplication.class, args);
-	}
-	
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder 
 	   application) {
 	      return application.sources(IntranetApplication.class);
 	   }
+	
+	
+	public static void main (String[] args) {
+		SpringApplication.run(IntranetApplication.class, args);
+	}
+	
 	
 	
 
